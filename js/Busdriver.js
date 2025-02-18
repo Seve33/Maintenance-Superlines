67a9fd95f3ee016b7315de2eef66e293442af318
M1R4C13 <62825708+Dianrs111@users.noreply.github.com>
2025-02-18 22:50:03 +0800

    Add files via upload

--- js/Busdriver.js
+++ js/Busdriver.js
@@ -1,18 +1,35 @@
-const busInfoBtn = document.getElementById('bus-info-btn');
-const reportBusBtn = document.getElementById('report-bus-btn');
-const busInfoSection = document.getElementById('bus-info-section');
-const reportBusSection = document.getElementById('report-bus-section');
+document.addEventListener("DOMContentLoaded", function () {
+  sessionStorage.setItem("Status", "Not Login");
+  const webStatus = sessionStorage.getItem("Status");
 
-busInfoBtn.addEventListener('click', () => {
-    busInfoSection.classList.remove('hidden');
-    reportBusSection.classList.add('hidden');
-    busInfoBtn.classList.add('active');
-    reportBusBtn.classList.remove('active');
+  if (webStatus !== "Login") {
+    Window.location.replace("Login.html");
+  } else {
+    const userName = sessionStorage.getItem("username");
+
+    if (userName !== null) {
+      document.getElementById("BusdriverName").innerHTML = userName;
+    } else {
+      document.getElementById("BusdriverName").innerHTML = "Bus Driver";
+    }
+  }
 });
 
-reportBusBtn.addEventListener('click', () => {
-    reportBusSection.classList.remove('hidden');
-    busInfoSection.classList.add('hidden');
-    reportBusBtn.classList.add('active');
-    busInfoBtn.classList.remove('active');
+const busInfoBtn = document.getElementById("bus-info-btn");
+const reportBusBtn = document.getElementById("report-bus-btn");
+const busInfoSection = document.getElementById("bus-info-section");
+const reportBusSection = document.getElementById("report-bus-section");
+
+busInfoBtn.addEventListener("click", () => {
+  busInfoSection.classList.remove("hidden");
+  reportBusSection.classList.add("hidden");
+  busInfoBtn.classList.add("active");
+  reportBusBtn.classList.remove("active");
+});
+
+reportBusBtn.addEventListener("click", () => {
+  reportBusSection.classList.remove("hidden");
+  busInfoSection.classList.add("hidden");
+  reportBusBtn.classList.add("active");
+  busInfoBtn.classList.remove("active");
 });
